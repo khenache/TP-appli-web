3ebdd93be047c6ded050ef8a6efddb3fbf3d7be4
khenache <72736567+khenache@users.noreply.github.com>
2020-11-02 17:11:40 +0100

    Update 1.js

--- tp2/1.js
+++ tp2/1.js
@@ -1,24 +1,36 @@
-function validation(){
+  function validation(){
   
-    var city = document.getElementById("city").value;
+    var Nom = document.getElementById("Nom").value;
     
-    if(city.length < 5){
+    if(Nom.length < 5){
       // afficher une erreur
       
     }
     
     // ou faire une validation de tous les champs en une seule fois
-    let error = "";
-  for(let i of document.querySelectorAll('.inputAW')){
-      console.log( i.value.length, i.getAttribute('maxlength'))
-      // afficher une erreur 
-      // si i.value.length < 5 && i.value.length > i.getAttribute('maxlength')
-  }
+    document.getElementById("error").innerHTML = "";
     
-    // ajouter / supprimer la classe CSS display
-    document.getElementById("error").classList.remove("display")
+  for(let item of document.querySelectorAll('.inputAW')){
+    if(item.value.length < 5){
+      // recuperation du label
+      var label =   document.querySelector("label[for="+item.id+"]").textContent;
+      // ajout du message d'erreur
+      document.getElementById("error").innerHTML += `Minimum 5 caractères pour : ${label} <br/>`;
+      
+      // ajout de la classe CSS "display"
+      document.getElementById("error").classList.add("display");
+        document.getElementById("resultat").classList.remove("display")
   
-    document.getElementById("error").classList.add("display")
+    }
+      
+  }
+    if(document.getElementById("error").innerHTML == ""){
+      var city = document.getElementById("Nom").value
+      document.getElementById("resultat").innerHTML = `Bienvenue à ${Nom}`;
+      document.getElementById("error").classList.remove("display")
+    document.getElementById("resultat").classList.add("display")
+    }
+     
   
     
   }
