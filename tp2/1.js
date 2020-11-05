30dc3967f3c38fdeacd4491b4ada57ed8d25a4ca
khenache <72736567+khenache@users.noreply.github.com>
2020-11-05 02:07:46 +0100

    update

--- tp2/1.js
+++ tp2/1.js
@@ -1,36 +1,28 @@
-  function validation(){
+function validation(){
+    
+  // ou faire une validation de tous les champs en une seule fois
+  document.getElementById("error").innerHTML = "";
   
-    var Nom = document.getElementById("Nom").value;
-    
-    if(Nom.length < 5){
-      // afficher une erreur
-      
-    }
-    
-    // ou faire une validation de tous les champs en une seule fois
-    document.getElementById("error").innerHTML = "";
-    
-  for(let item of document.querySelectorAll('.inputAW')){
-    if(item.value.length < 5){
-      // recuperation du label
+for(let item of document.querySelectorAll('.inputAW')){
+  if(item.value == ""){
+
       var label =   document.querySelector("label[for="+item.id+"]").textContent;
-      // ajout du message d'erreur
-      document.getElementById("error").innerHTML += `Minimum 5 caractères pour : ${label} <br/>`;
-      
-      // ajout de la classe CSS "display"
+      document.getElementById("error").innerHTML += ` ce champ est obligatoire pour : ${label} <br/>`;
       document.getElementById("error").classList.add("display");
-        document.getElementById("resultat").classList.remove("display")
-  
-    }
-      
+      document.getElementById("resultat").classList.remove("display")
+
+  }else if(item.value.length < 5){
+      var label =   document.querySelector("label[for="+item.id+"]").textContent;
+      document.getElementById("error").innerHTML += `Minimum 5 caractères pour : ${label} <br/>`;
+      document.getElementById("error").classList.add("display");
+      document.getElementById("resultat").classList.remove("display")
   }
-    if(document.getElementById("error").innerHTML == ""){
-      var city = document.getElementById("Nom").value
-      document.getElementById("resultat").innerHTML = `Bienvenue à ${Nom}`;
-      document.getElementById("error").classList.remove("display")
-    document.getElementById("resultat").classList.add("display")
-    }
-     
-  
-    
-  }
+}
+if(document.getElementById("error").innerHTML == ""){
+  var nm = document.getElementById("nom").value
+  document.getElementById("resultat").innerHTML = `Bienvenue ${nm}`;
+  document.getElementById("error").classList.remove("display")
+  document.getElementById("resultat").classList.add("display")
+}
+
+}
