a77878ef2b4b96f02d3e6a88067677f9b872628e
khenache <72736567+khenache@users.noreply.github.com>
2020-11-08 01:50:03 +0100

    update

--- tp2/Programme.js
+++ tp2/Programme.js
@@ -6,20 +6,20 @@ for(let item of document.querySelectorAll('.inputAW')){
   if(item.value == ""){
 
       var label =   document.querySelector("label[for="+item.id+"]").textContent;
-      document.getElementById("error").innerHTML += ` ce champ est obligatoire pour : ${label} <br/>`;
+      document.getElementById("error").innerHTML += ` ce champ est obligatoire: ${label} <br/>`;
       document.getElementById("error").classList.add("display");
       document.getElementById("resultat").classList.remove("display")
 
   }else if(item.value.length < 5){
       var label =   document.querySelector("label[for="+item.id+"]").textContent;
-      document.getElementById("error").innerHTML += `Minimum 5 caractères pour : ${label} <br/>`;
+      document.getElementById("error").innerHTML += `Ce champ doit comporter au Minimum 5 caractères : ${label} <br/>`;
       document.getElementById("error").classList.add("display");
       document.getElementById("resultat").classList.remove("display")
   }
 }
 if(document.getElementById("error").innerHTML == ""){
   var nm = document.getElementById("nom").value
-  document.getElementById("resultat").innerHTML = `Bienvenue ${nm}`;
+  document.getElementById("resultat").innerHTML = `bienvenue sur le formulaire ${nm}`;
   document.getElementById("error").classList.remove("display")
   document.getElementById("resultat").classList.add("display")
 }
