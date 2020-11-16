4bbc92d17cb38194de79826e169b2400968cee72
khenache <72736567+khenache@users.noreply.github.com>
2020-11-16 15:30:56 +0100

    mise a jour

--- tp3/js/form-jquery-validation.js
+++ tp3/js/form-jquery-validation.js
@@ -15,13 +15,13 @@ $(document).ready(function () {
             var city = $("#adresse").val();
             var date = $("#date").val();
 
-            $(".modal-body").html(`Vous êtes nés ${date} et vous habitez :
+            $(".modal-body").html( `Vous êtes nés ${date} et vous habitez :
             <a class="link"  href="http://maps.google.com/maps?" ><img src="https://maps.googleapis.com/maps/api/staticmap?markers=${city}&zoom=14&size=400x300&scale=2&key=AIzaSyAkmvI9DazzG9p77IShsz_Di7-5Qn7zkcg"/> Lien Google Maps </a>`);
 
         } else {
 
             $('#myModal').modal({ show: true })
-            $(".modal-body").html('Veuillez remplire tout les champ obligatoire');
+            $(".modal-body").html('Veuillez remplire tout les champs obligatoire');
 
         }
     });
